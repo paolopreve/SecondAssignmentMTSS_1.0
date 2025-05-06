c4a5bd742e1fb238d34afe609053a5bcf92a9847
aldo_bettega <aldo.bettega@studenti.unipd.it>
2025-05-06 10:46:41 +0200

    Configurato analisi statica. Close #2

--- roman-number/src/test/java/unipt/it/mtss/AppTest.java
+++ roman-number/src/test/java/unipt/it/mtss/AppTest.java
@@ -1,3 +1,7 @@
+/////////////////////////////////////////////
+// ALDO BETTEGA 2101087
+// PAOLO PREVEDELLO RAMIREZ 2111944
+////////////////////////////////////////////
 package unipt.it.mtss;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
